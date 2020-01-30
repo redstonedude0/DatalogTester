27a51ba4104a9108490d487418d599f3135a6a58
harrison <hrjh2@cam.ac.uk>
2020-01-30 11:22:56 +0000

    example test framework

---
+++ src/uk/ac/cam/gp/charlie/datalog/GraqlInterpreter.java
@@ -0,0 +1,17 @@
+package uk.ac.cam.gp.charlie.datalog;
+
+public class GraqlInterpreter {
+
+  public static String toDatalog(String graql) {
+    //TODO:
+
+    //tokenise graql
+
+    //parse into AST
+
+    //compile into datalog
+
+    return graql;
+  }
+
+}
