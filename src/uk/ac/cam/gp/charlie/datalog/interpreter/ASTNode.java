89af6b1db3f9824041158672272e75799784d759
harrison <hrjh2@cam.ac.uk>
2020-01-30 11:55:46 +0000

    brief outline of how a context should work

--- src/uk/ac/cam/gp/charlie/datalog/interpreter/ASTNode.java
+++ src/uk/ac/cam/gp/charlie/datalog/interpreter/ASTNode.java
@@ -2,6 +2,4 @@ package uk.ac.cam.gp.charlie.datalog.interpreter;
 
 public class ASTNode {
 
-  public class DefineEntity
-
 }
--- src/uk/ac/cam/gp/charlie/datalog/interpreter/Context.java
+++ src/uk/ac/cam/gp/charlie/datalog/interpreter/Context.java
@@ -3,5 +3,77 @@ package uk.ac.cam.gp.charlie.datalog.interpreter;
 public class Context {
 
   //Stores a schema context in AST form
+  //TODO needs to transform
+  /**
+   * define
+   *
+   * person sub entity,
+   *   has name,
+   *   plays employee;
+   *
+   * organisation sub entity,
+   *   has name,
+   *   plays employer;
+   *
+   * employment sub relation,
+   *   relates employee,
+   *   relates employer;
+   *
+   * coworkers sub relation,
+   *    relates employee;
+   *
+   * same-employment-are-coworkers sub rule,
+   *   when {
+   *     (employee: $x, $z) isa employment;
+   *     (employee: $y, $z) isa employment;
+   *     $x != $y;
+   *   }, then {
+   *     ($x,$y) isa coworkers;
+   *   };
+   * ###Data
+   * insert $x isa person has name "Bob";
+   * insert $y isa organisation has name "Uni"";
+   * insert $z (employer: $y, employee: $x) isa employment;
+   */
+
+  //INTO:
+  //List<Definition> Schema
+  //  [Definition->DefineEntity
+  //    List<String> attributes
+  //      ["name"]
+  //    List<String> roles
+  //      ["employee"]
+  //    String name "person"
+  //    String subs "entity"
+  //  [Definition->DefineEntity
+  //    List<String> attributes
+  //      ["name"]
+  //    List<String> roles
+  //      ["employer"]
+  //    String name "organisation"
+  //    String subs "entity"
+  //   [Definition->DefineRelation
+  //    List<String> relates [employee, employer]
+  //    String name "employment"
+  //    String subs "relation"
+  //  [Definition->DefineRule
+  //    String name "same-employment-are-coworkers"
+  //    String subs "rule"
+  //    List<Clause>
+  //      [Clause->Isa
+  //        String type "employment";
+  //        Map<String,List<String>>
+  //          employee->["x"]
+  //          employer->["z"]
+  //      [Clause->Isa
+  //        String type "employment";
+  //        Map<String,List<String>>
+  //          employee->["y"]
+  //          employer->["z"]
+  //      [Clause->NotEqual
+  //        LHS: Clause->Variable "x"
+  //        RHS: Clause->Variable "y"
+  //List<Insertion> Data
+
 
 }
