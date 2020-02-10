03d8bbc9121adc2f2e4d598e4373daba04b01109
harrison <hrjh2@cam.ac.uk>
2020-02-10 15:36:53 +0000

    unit tests for ASTInterpreter completed

--- src/uk/ac/cam/gp/charlie/datalog/tests/TESTAstInterpreter.java
+++ src/uk/ac/cam/gp/charlie/datalog/tests/TESTAstInterpreter.java
@@ -1,12 +1,27 @@
 package uk.ac.cam.gp.charlie.datalog.tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
 import abcdatalog.ast.Clause;
+import abcdatalog.ast.PositiveAtom;
+import com.google.common.collect.Lists;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.junit.Test;
+import uk.ac.cam.gp.charlie.TestEnvironment;
 import uk.ac.cam.gp.charlie.ast.Attribute;
+import uk.ac.cam.gp.charlie.ast.ConstantValue;
 import uk.ac.cam.gp.charlie.ast.Plays;
+import uk.ac.cam.gp.charlie.ast.Variable;
+import uk.ac.cam.gp.charlie.ast.queries.Query;
 import uk.ac.cam.gp.charlie.ast.queries.QueryDefine;
+import uk.ac.cam.gp.charlie.ast.queries.QueryInsert;
+import uk.ac.cam.gp.charlie.ast.queries.match.ConditionIsa;
+import uk.ac.cam.gp.charlie.ast.queries.match.QueryMatch;
 import uk.ac.cam.gp.charlie.datalog.interpreter.ASTInterpreter;
 import uk.ac.cam.gp.charlie.datalog.interpreter.Context;
 
@@ -19,7 +34,7 @@ public class TESTAstInterpreter {
       if (mutableDatalog.contains(c_raw)) {
         mutableDatalog.remove(c_raw);
       } else {
-        throw new RuntimeException("Datalog not equal:\n  Actual Clause:"+c_raw);
+        throw new RuntimeException("Datalog not equal:\n  Actual Clause:"+c_raw+"\n  Remaining Clauses:"+mutableDatalog);
       }
     }
     if (mutableDatalog.size() != 0) {
@@ -58,5 +73,252 @@ public class TESTAstInterpreter {
     //Assert
     assertDatalogEqual(s,datalog);
   }
+
+  @Test
+  public void test_defineEntities_advanced() {
+    //Arrange
+    Set<String> datalog = new HashSet<>();
+    QueryDefine q1 = new QueryDefine("person",QueryDefine.getFromIdentifier("entity"));
+    datalog.add("t_subs(t_0, entity).");
+    q1.attributes.add(Attribute.fromIdentifier("name"));
+    datalog.add("t_hasattr(t_0, a_0).");
+    q1.attributes.add(Attribute.fromIdentifier("nickname"));
+    datalog.add("t_hasattr(t_0, a_1).");
+    q1.plays.add(Plays.fromIdentifier("friend"));
+    datalog.add("t_playsrole(t_0, r_0).");
+    q1.plays.add(Plays.fromIdentifier("employee"));
+    datalog.add("t_playsrole(t_0, r_1).");
+
+    QueryDefine q2 = new QueryDefine("organisation",QueryDefine.getFromIdentifier("entity"));
+    datalog.add("t_subs(t_1, entity).");
+    q2.attributes.add(Attribute.fromIdentifier("name"));
+    datalog.add("t_hasattr(t_1, a_0).");
+    q2.attributes.add(Attribute.fromIdentifier("slogan"));
+    datalog.add("t_hasattr(t_1, a_2).");
+    q2.plays.add(Plays.fromIdentifier("market_agent"));
+    datalog.add("t_playsrole(t_1, r_2).");
+    q2.plays.add(Plays.fromIdentifier("employer"));
+    datalog.add("t_playsrole(t_1, r_3).");
+    Context c = new Context();
+    //Act
+    Set<Clause> s1 = ASTInterpreter.toDatalog(q1,c);
+    Set<Clause> s2 = ASTInterpreter.toDatalog(q2,c);
+    s1.addAll(s2);
+    //Assert
+    assertDatalogEqual(s1,datalog);
+  }
+
+  @Test
+  public void test_defineMultiple_advanced() {
+    //Arrange
+    Set<String> datalog = new HashSet<>();
+    QueryDefine q1 = new QueryDefine("person",QueryDefine.getFromIdentifier("entity"));
+    datalog.add("t_subs(t_0, entity).");
+    q1.attributes.add(Attribute.fromIdentifier("name"));
+    datalog.add("t_hasattr(t_0, a_0).");
+    q1.attributes.add(Attribute.fromIdentifier("nickname"));
+    datalog.add("t_hasattr(t_0, a_1).");
+    q1.plays.add(Plays.fromIdentifier("friend"));
+    datalog.add("t_playsrole(t_0, r_0).");
+    q1.plays.add(Plays.fromIdentifier("employee"));
+    datalog.add("t_playsrole(t_0, r_1).");
+
+    QueryDefine q2 = new QueryDefine("organisation",QueryDefine.getFromIdentifier("entity"));
+    datalog.add("t_subs(t_1, entity).");
+    q2.attributes.add(Attribute.fromIdentifier("name"));
+    datalog.add("t_hasattr(t_1, a_0).");
+    q2.attributes.add(Attribute.fromIdentifier("slogan"));
+    datalog.add("t_hasattr(t_1, a_2).");
+    q2.plays.add(Plays.fromIdentifier("market_agent"));
+    datalog.add("t_playsrole(t_1, r_2).");
+    q2.plays.add(Plays.fromIdentifier("employer"));
+    datalog.add("t_playsrole(t_1, r_3).");
+
+    QueryDefine q3 = new QueryDefine("coworkers",QueryDefine.getFromIdentifier("relation"));
+    datalog.add("t_subs(t_2, relation).");
+    q3.relates.add(Plays.fromIdentifier("employee"));
+    datalog.add("t_relates(t_2, r_1).");
+
+    QueryDefine q4 = new QueryDefine("friends",QueryDefine.getFromIdentifier("relation"));
+    datalog.add("t_subs(t_3, relation).");
+    q4.relates.add(Plays.fromIdentifier("friend"));
+    datalog.add("t_relates(t_3, r_0).");
+
+    Context c = new Context();
+    //Act
+    Set<Clause> s1 = ASTInterpreter.toDatalog(q1,c);
+    Set<Clause> s2 = ASTInterpreter.toDatalog(q2,c);
+    Set<Clause> s3 = ASTInterpreter.toDatalog(q3,c);
+    Set<Clause> s4 = ASTInterpreter.toDatalog(q4,c);
+    s1.addAll(s2);
+    s1.addAll(s3);
+    s1.addAll(s4);
+    //Assert
+    assertDatalogEqual(s1,datalog);
+  }
+
+  private Context setupTestEnv1() {
+    List<Query> toRun = new ArrayList<>();
+
+    //
+    //SCHEMA
+    //
+    Attribute nameAttribute = Attribute.fromIdentifier("name");
+
+    Plays employee = Plays.fromIdentifier("employee");
+//    Plays ceo = Plays.fromIdentifier("ceo");
+    Plays employer = Plays.fromIdentifier("employer");
+    Plays friend = Plays.fromIdentifier("friend");
+//    Plays lover = Plays.fromIdentifier("lover");
+
+    QueryDefine person = new QueryDefine("person",QueryDefine.getFromIdentifier("entity"));
+    person.attributes.add(nameAttribute);
+    person.plays.add(employee);
+    person.plays.add(friend);
+    toRun.add(person);
+
+    QueryDefine organisation = new QueryDefine("organisation",QueryDefine.getFromIdentifier("entity"));
+    organisation.attributes.add(nameAttribute);
+    organisation.plays.add(employer);
+    toRun.add(organisation);
+
+    QueryDefine employment = new QueryDefine("employment",QueryDefine.getFromIdentifier("relation"));
+    employment.relates.add(employee);
+    employment.relates.add(employer);
+    toRun.add(employment);
+
+    QueryDefine coworkers = new QueryDefine("coworkers",QueryDefine.getFromIdentifier("relation"));
+    coworkers.relates.add(employee);
+    toRun.add(coworkers);
+
+    QueryDefine friendship = new QueryDefine("friendship",QueryDefine.getFromIdentifier("relation"));
+    friendship.relates.add(friend);
+    toRun.add(friendship);
+    //
+    // DATA
+    //
+    Variable var_y = Variable.fromIdentifier("y");
+    Variable var_x = Variable.fromIdentifier("x");
+    Variable var_z = Variable.fromIdentifier("z");
+
+    QueryInsert insert = new QueryInsert(var_y, "organisation");
+    insert.attributes.put(Attribute.fromIdentifier("name"), ConstantValue.fromValue("Uni"));
+    toRun.add(insert);
+
+    insert = new QueryInsert(var_x, "person");
+    insert.attributes.put(Attribute.fromIdentifier("name"), ConstantValue.fromValue("Alice"));
+    toRun.add(insert);
+
+    insert = new QueryInsert(var_x, "person");
+    insert.attributes.put(Attribute.fromIdentifier("name"), ConstantValue.fromValue("Bob"));
+    toRun.add(insert);
+
+    insert = new QueryInsert(var_x, "person");
+    insert.attributes.put(Attribute.fromIdentifier("name"), ConstantValue.fromValue("Charlie"));
+    toRun.add(insert);
+
+    insert = new QueryInsert(var_x, "person");
+    insert.attributes.put(Attribute.fromIdentifier("name"), ConstantValue.fromValue("Dhruv"));
+    toRun.add(insert);
+
+    insert = new QueryInsert(var_x, "person");
+    insert.attributes.put(Attribute.fromIdentifier("name"), ConstantValue.fromValue("Ellie"));
+    toRun.add(insert);
+
+
+    insert = new QueryInsert(var_z, "employment"); //employ ellie at uni
+    insert.plays.add(new SimpleEntry<>(Plays.fromIdentifier("employer"), var_y));
+    insert.plays.add(new SimpleEntry<>(Plays.fromIdentifier("employee"), var_x));
+    toRun.add(insert);
+
+    //Make alice and bob friends
+    QueryInsert insertQuery = new QueryInsert(var_z, "friendship");
+    insertQuery.plays.add(new SimpleEntry<>(Plays.fromIdentifier("friend"), var_x));
+    insertQuery.plays.add(new SimpleEntry<>(Plays.fromIdentifier("friend"), var_y));
+    QueryMatch match = new QueryMatch();
+    match.setActionInsert(insertQuery);
+    ConditionIsa cond_1 = new ConditionIsa(var_x, "person");
+    cond_1.has.put(Attribute.fromIdentifier("name"), ConstantValue.fromValue("Bob"));
+    ConditionIsa cond_2 = new ConditionIsa(var_y, "person");
+    cond_2.has.put(Attribute.fromIdentifier("name"), ConstantValue.fromValue("Alice"));
+    match.conditions.add(cond_1);
+    match.conditions.add(cond_2);
+    toRun.add(match);
+
+    //Make everyone coworkers
+    insertQuery = new QueryInsert(var_z, "coworkers");
+    insertQuery.plays.add(new SimpleEntry<>(Plays.fromIdentifier("employee"), var_x));
+    insertQuery.plays.add(new SimpleEntry<>(Plays.fromIdentifier("employee"), var_y));
+    match = new QueryMatch();
+    match.setActionInsert(insertQuery);
+    cond_1 = new ConditionIsa(var_x, "person");
+    cond_2 = new ConditionIsa(var_y, "person");
+    match.conditions.add(cond_1);
+    match.conditions.add(cond_2);
+    toRun.add(match);
+
+    Context c = new Context();
+    for(Query q : toRun) {
+      ASTInterpreter.toDatalog(q,c);
+    }
+    return c;
+  }
+
+  @Test
+  public void test_env1_peopleExist() {
+    //Arrange
+    Context c = setupTestEnv1();
+    Set<String> datalog = new HashSet<>();
+
+    Variable var_n = Variable.fromIdentifier("n");
+    Variable var_x = Variable.fromIdentifier("x");
+    QueryMatch q = new QueryMatch();
+    q.setActionGet(Lists.asList(var_n,new Variable[0]));
+    ConditionIsa cond = new ConditionIsa(var_x,"person");
+    cond.has.put(Attribute.fromIdentifier("name"),var_n);
+    q.conditions.add(cond);
+    datalog.add("query(Var0, Var1) :- instanceof(Var0, t_0), instanceattr(Var0, a_0, Var1).");
+    //Act
+    Set<Clause> s = ASTInterpreter.toDatalog(q,c);
+    PositiveAtom pa = ASTInterpreter.toExecutableDatalog(q,c);
+    //Assert
+    assertDatalogEqual(s,datalog);
+    assertEquals(pa.toString(),"query(Var0, Var1)");
+  }
+
+  /*
+  //match get
+  Variable var_n = Variable.fromIdentifier("n");
+  QueryMatch matchget = new QueryMatch();
+    matchget.setActionGet(Lists.asList(var_n,new Variable[0]));
+  cond_1 = new ConditionIsa(var_x,"person");
+    cond_1.has.put(Attribute.fromIdentifier("name"),var_n);
+    matchget.conditions.add(cond_1);
+    toRun.add(matchget);
+
+  //match delete  match $x isa person, has name "Alice"; $y (employee:$x) isa employment; delete $y;
+  QueryMatch matchdel = new QueryMatch();
+    matchdel.setActionDelete(var_y);
+  cond_1 = new ConditionIsa(var_x, "person");
+    cond_1.has.put(Attribute.fromIdentifier("name"),ConstantValue.fromValue("Alice"));
+  cond_2 = new ConditionIsa(var_y,"employment");
+    cond_2.relates.add(new SimpleEntry<>(Plays.fromIdentifier("employee"),var_x));
+    matchdel.conditions.add(cond_1);
+    matchdel.conditions.add(cond_2);
+  //toRet.add(matchdel);
+
+
+  //match delete  match $x isa person, has name "Alice"; delete $x;
+  matchdel = new QueryMatch();
+    matchdel.setActionDelete(var_x);
+  cond_1 = new ConditionIsa(var_x, "person");
+    cond_1.has.put(Attribute.fromIdentifier("name"),ConstantValue.fromValue("Alice"));
+    matchdel.conditions.add(cond_1);
+    toRun.add(matchdel);
+
+
+  //match get again
+    toRun.add(matchget);
+*/
 
 }
--- src/uk/ac/cam/gp/charlie/graql/GraqlParser.java
+++ src/uk/ac/cam/gp/charlie/graql/GraqlParser.java
@@ -1,12 +1,9 @@
 package uk.ac.cam.gp.charlie.graql;
 
-import com.google.common.collect.Lists;
-import com.google.errorprone.annotations.Var;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import org.w3c.dom.Attr;
 import uk.ac.cam.gp.charlie.ast.Attribute;
 import uk.ac.cam.gp.charlie.ast.ConstantValue;
 import uk.ac.cam.gp.charlie.ast.Plays;
@@ -14,8 +11,6 @@ import uk.ac.cam.gp.charlie.ast.Variable;
 import uk.ac.cam.gp.charlie.ast.queries.Query;
 import uk.ac.cam.gp.charlie.ast.queries.QueryDefine;
 import uk.ac.cam.gp.charlie.ast.queries.QueryInsert;
-import uk.ac.cam.gp.charlie.ast.queries.match.ConditionIsa;
-import uk.ac.cam.gp.charlie.ast.queries.match.QueryMatch;
 
 /**
  * Parse Graql into ASTs
@@ -23,15 +18,6 @@ import uk.ac.cam.gp.charlie.ast.queries.match.QueryMatch;
 public class GraqlParser {
 
   public static List<Query> graqlToAST(String input) {
-    //TODO for now leave this in so it's easier to test AST->datalog directly
-    switch (input) {
-      case "test_schema":
-        return test_schema();
-      case "test_data":
-        return test_data();
-    }
-    //TODO remove above once we've finished AST stuff
-
     //parse input into 0 or more graql statements, and return then
     List<Query> queryList = new ArrayList<>();
     String[] typeQuery = input.split("\n", 2);
@@ -119,195 +105,6 @@ public class GraqlParser {
   public static void main(String[] args) {
     String test = "insert\n$z (employee: $x, employee: $y) isa coworkers;";
     GraqlParser.graqlToAST(test);
-  }
-
-  /**
-   * ############################################################# I suggest we remove all code
-   * below, in favour of just using the code above.
-   */
-
-  private static List<Query> test_data() {
-    List<Query> toRet = new ArrayList<>();
-    //this returns an example list of inserts
-    /**
-     * The below example is equivalent to the graql:
-     *
-     * insert $x isa person has name "Bob";
-     * insert $y isa organisation has name "Uni"";
-     * insert $z (employer: $y, employee: $x) isa employment;
-     * insert $x isa person has name "Alice";
-     * insert $z (employer: $y, employee: $x) isa employment;
-     *
-     * match
-     *  $x isa person has name "Bob";
-     *  $y isa person has name "Alice";
-     * insert
-     *  $z (employee: $x, employee: $y) isa coworkers;
-     *
-     * //TODO
-     * match
-     *  $x isa person has name $n;
-     * get $n;
-     *
-     */
-
-    Variable var_y = Variable.fromIdentifier("y");
-    QueryInsert uni = new QueryInsert(var_y, "organisation");
-    uni.attributes.put(Attribute.fromIdentifier("name"), ConstantValue.fromValue("Uni"));
-    toRet.add(uni);
-
-    Variable var_x = Variable.fromIdentifier("x");
-    QueryInsert bob = new QueryInsert(var_x, "person");
-    bob.attributes.put(Attribute.fromIdentifier("name"), ConstantValue.fromValue("Bob"));
-    toRet.add(bob);
-
-    Variable var_z = Variable.fromIdentifier("z");
-    QueryInsert z = new QueryInsert(var_z, "employment");
-    z.plays.add(new SimpleEntry<>(Plays.fromIdentifier("employer"), var_y));
-    z.plays.add(new SimpleEntry<>(Plays.fromIdentifier("employee"), var_x));
-    toRet.add(z);
-
-    //Redefinition of var_x isn't necessary - fromIdentifier will return the same object,
-    //I've just included it here for consistency, it's up to you whether you do this or not
-    var_x = Variable.fromIdentifier("x");
-    QueryInsert alice = new QueryInsert(var_x, "person");
-    alice.attributes.put(Attribute.fromIdentifier("name"), ConstantValue.fromValue("Alice"));
-    toRet.add(alice);
-
-    //Example without redefinition of var and new query obj.
-    z = new QueryInsert(var_z, "employment");
-    z.plays.add(new SimpleEntry<>(Plays.fromIdentifier("employer"), var_y));
-    z.plays.add(new SimpleEntry<>(Plays.fromIdentifier("employee"), var_x));
-    toRet.add(z);
-
-    QueryInsert person = new QueryInsert(var_x, "person");
-    person.attributes.put(Attribute.fromIdentifier("name"), ConstantValue.fromValue("Charlie"));
-    toRet.add(person);
-    person = new QueryInsert(var_x, "person");
-    person.attributes.put(Attribute.fromIdentifier("name"), ConstantValue.fromValue("Dhruv"));
-    toRet.add(person);
-    person = new QueryInsert(var_x, "person");
-    person.attributes.put(Attribute.fromIdentifier("name"), ConstantValue.fromValue("Ellie"));
-    toRet.add(person);
-
-
-    //Examples of match
-    QueryInsert insertQuery = new QueryInsert(var_z, "friendship");
-    insertQuery.plays.add(new SimpleEntry<>(Plays.fromIdentifier("friend"), var_x));
-    insertQuery.plays.add(new SimpleEntry<>(Plays.fromIdentifier("friend"), var_y));
-    QueryMatch match = new QueryMatch();
-    match.setActionInsert(insertQuery);
-    ConditionIsa cond_1 = new ConditionIsa(var_x, "person");
-    cond_1.has.put(Attribute.fromIdentifier("name"), ConstantValue.fromValue("Bob"));
-    ConditionIsa cond_2 = new ConditionIsa(var_y, "person");
-    cond_2.has.put(Attribute.fromIdentifier("name"), ConstantValue.fromValue("Alice"));
-    match.conditions.add(cond_1);
-    match.conditions.add(cond_2);
-    toRet.add(match);
-
-    //Examples of match
-    insertQuery = new QueryInsert(var_z, "coworkers");
-    insertQuery.plays.add(new SimpleEntry<>(Plays.fromIdentifier("employee"), var_x));
-    insertQuery.plays.add(new SimpleEntry<>(Plays.fromIdentifier("employee"), var_y));
-    match = new QueryMatch();
-    match.setActionInsert(insertQuery);
-    cond_1 = new ConditionIsa(var_x, "person");
-    cond_2 = new ConditionIsa(var_y, "person");
-    match.conditions.add(cond_1);
-    match.conditions.add(cond_2);
-    toRet.add(match);
-
-    //match get
-    Variable var_n = Variable.fromIdentifier("n");
-    QueryMatch matchget = new QueryMatch();
-    matchget.setActionGet(Lists.asList(var_n,new Variable[0]));
-    cond_1 = new ConditionIsa(var_x,"person");
-    cond_1.has.put(Attribute.fromIdentifier("name"),var_n);
-    matchget.conditions.add(cond_1);
-    toRet.add(matchget);
-
-    //match delete  match $x isa person, has name "Alice"; $y (employee:$x) isa employment; delete $y;
-    QueryMatch matchdel = new QueryMatch();
-    matchdel.setActionDelete(var_y);
-    cond_1 = new ConditionIsa(var_x, "person");
-    cond_1.has.put(Attribute.fromIdentifier("name"),ConstantValue.fromValue("Alice"));
-    cond_2 = new ConditionIsa(var_y,"employment");
-    cond_2.relates.add(new SimpleEntry<>(Plays.fromIdentifier("employee"),var_x));
-    matchdel.conditions.add(cond_1);
-    matchdel.conditions.add(cond_2);
-    //toRet.add(matchdel);
-
-
-    //match delete  match $x isa person, has name "Alice"; delete $x;
-    matchdel = new QueryMatch();
-    matchdel.setActionDelete(var_x);
-    cond_1 = new ConditionIsa(var_x, "person");
-    cond_1.has.put(Attribute.fromIdentifier("name"),ConstantValue.fromValue("Alice"));
-    matchdel.conditions.add(cond_1);
-    toRet.add(matchdel);
-
-
-    //match get again
-    toRet.add(matchget);
-
-    return toRet;
-  }
-
-  private static List<Query> test_schema() {
-    List<Query> toRet = new ArrayList<>();
-    //this returns an example list of defines
-    /**
-     * The below example is equivalent to the graql:
-     *
-     * define
-     *
-     * person sub entity,
-     *   has name,
-     *   plays employee;
-     *
-     * organisation sub entity,
-     *   has name,
-     *   plays employer;
-     *
-     * employment sub relation,
-     *   relates employee,
-     *   relates employer;
-     *
-     * coworkers sub relation,
-     *   relates employee;
-     *
-     */
-
-    Attribute nameAttribute = Attribute.fromIdentifier("name");
-
-    Plays employee = Plays.fromIdentifier("employee");
-    Plays employer = Plays.fromIdentifier("employer");
-    Plays friend = Plays.fromIdentifier("friend");
-
-    QueryDefine person = new QueryDefine("person",QueryDefine.getFromIdentifier("entity"));
-    person.attributes.add(nameAttribute);
-    person.plays.add(employee);
-    person.plays.add(friend);
-    toRet.add(person);
-
-    QueryDefine organisation = new QueryDefine("organisation",QueryDefine.getFromIdentifier("entity"));
-    organisation.attributes.add(nameAttribute);
-    organisation.plays.add(employer);
-    toRet.add(organisation);
-
-    QueryDefine employment = new QueryDefine("employment",QueryDefine.getFromIdentifier("relation"));
-    employment.relates.add(employee);
-    employment.relates.add(employer);
-    toRet.add(employment);
-
-    QueryDefine coworkers = new QueryDefine("coworkers",QueryDefine.getFromIdentifier("relation"));
-    coworkers.relates.add(employee);
-    toRet.add(coworkers);
-
-    QueryDefine friendship = new QueryDefine("friendship",QueryDefine.getFromIdentifier("relation"));
-    friendship.relates.add(friend);
-    toRet.add(friendship);
-    return toRet;
   }
 
 }
