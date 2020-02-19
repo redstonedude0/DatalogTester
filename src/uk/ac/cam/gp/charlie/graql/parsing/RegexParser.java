0fe3dbc882ce048c3df978d71214a6fd379864d3
unknown <charlie990121@gmail.com>
2020-02-19 14:32:31 +0200

    commit in order to merge

--- src/uk/ac/cam/gp/charlie/graql/parsing/RegexParser.java
+++ src/uk/ac/cam/gp/charlie/graql/parsing/RegexParser.java
@@ -3,6 +3,7 @@ package uk.ac.cam.gp.charlie.graql.parsing;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,7 +13,27 @@ import uk.ac.cam.gp.charlie.ast.queries.QueryDefine;
 
 public class RegexParser extends GraqlParser {
 
+  public static void main(String[] args) {
+    iterate(matcher("$a", regex("<test>"))).forEachRemaining(new Consumer<MatchResult>() {
+      @Override
+      public void accept(MatchResult matchResult) {
+        System.out.println(matchResult.start()+":"+matchResult.end());
+      }
+    });
+  }
+
   private static String regex(String regex) {
+    regex = regex.replaceAll("<insert_block>","(<wso>insert(<ws><insert>)+)");
+    regex = regex.replaceAll("<insert>", "(((?<INSERTHEADIDENT><variable_identifier>)<ws>isa<ws>(?<INSERTHEADISA><identifier>))(<insert_has>)*<wso>;)");
+    regex = regex.replaceAll("<insert_has>", "(<wso>,<ws>has<ws>(?<INSERTHASIDENT><identifier><ws><wso><property_identifier>))");
+    regex = regex.replaceAll("<variable_identifier>","(\\$<alpha>(<alphanum>)+)");
+    regex = regex.replaceAll("<property_identifier>","(\"<alpha>(<alphanumspace>)+)\"");
+    regex = regex.replaceAll("<alphanumspace>","(<alpha>|<num>|-|<ws>|=)");
+    regex = regex.replaceAll("<test1>", "[(]<identifier>:<ws>");
+    regex = regex.replaceAll("<test>", "\\$a");
+
+
+
     regex = regex.replaceAll("<define_block>","(<wso>define(<ws><define>)+)");
     regex = regex.replaceAll("<define>","(((?<DEFINEHEADIDENT><identifier>)<ws>sub<ws>(?<DEFINEHEADSUBS><identifier>))(<define_has>|<define_plays>|<define_relates>)*<wso>;)");
     regex = regex.replaceAll("<define_relates>","(<wso>,<ws>relates<ws><identifier>)"); //'relates' definition
