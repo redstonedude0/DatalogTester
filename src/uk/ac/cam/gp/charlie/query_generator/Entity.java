27c215ce86e2e29a72b77c8842079656000dcf04
gcorso <gabri.corso@gmail.com>
2020-02-20 11:46:43 +0000

    Added query generator for insert queries

--- src/uk/ac/cam/gp/charlie/query_generator/Entity.java
+++ src/uk/ac/cam/gp/charlie/query_generator/Entity.java
@@ -1,19 +1,19 @@
 package uk.ac.cam.gp.charlie.query_generator;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Entity {
     String name;
     Set<Entity> closure;
     Entity parent;
+    List<String> instances;
 
-    public Entity(String name) {
+
+    Entity(String name) {
         this.name = name;
         closure = new HashSet<>();
         closure.add(this);
+        instances = new ArrayList<>();
     }
 
     private void isParent(Entity child){
@@ -21,9 +21,29 @@ public class Entity {
         if(parent != null) parent.isParent(child);
     }
 
-    public void isChild(Entity parent){
+    void isChild(Entity parent){
         if (equals(parent)) return;
         this.parent = parent;
         parent.isParent(this);
+    }
+
+    void addInstance(String name){
+        instances.add(name);
+    }
+
+    String getRandomInstance(){
+        Random random = new Random();
+        Entity e = getRandomClosureEntity();
+        int ninstance = random.nextInt(e.instances.size());
+        return e.instances.get(ninstance);
+    }
+
+    private Entity getRandomClosureEntity(){
+        Random random = new Random();
+        int n = random.nextInt(closure.size());
+        for (Entity e : closure) {
+            if (n-- == 0) return e;
+        }
+        return null;
     }
 }
